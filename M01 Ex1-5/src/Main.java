3935f7c593a8f8c2a1ea8ad7ccb56a828b682b88
jbenson39 <jbenson39@ivytech.edu>
2019-09-06 10:57:49 -0400

    M01 Programming assignment, exercise 1.5

---
+++ M01 Ex1-5/src/Main.java
@@ -0,0 +1,12 @@
+//  Joseph Benson     06 Sept 2019
+//  M01 Programming Assignment - Exercise 1.5  (Liang p31)
+//  Compute expression
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+
+
+    }
+}
