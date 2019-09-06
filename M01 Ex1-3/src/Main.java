533f34252d2efbf96893661d7a9bfd6de5c33dd2
jbenson39 <jbenson39@ivytech.edu>
2019-09-06 10:45:01 -0400

    M01 Programming assignment, exercise 1.3

---
+++ M01 Ex1-3/src/Main.java
@@ -0,0 +1,14 @@
+//  Joseph Benson     06 Sept 2019
+//  M01 Programming Assignment - Exercise 1.3  (Liang p30)
+//  Display a pattern
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("    J     A     V     V     A");
+        System.out.println("    J    A A     V   V     A A");
+        System.out.println("J   J   AAAAA     V V     AAAAA");
+        System.out.println(" J J   A     A     V     A     A");
+
+    }
+}
