59c66c4b6c4a42ce0c0c9b9cbc2432ddedca2288
jbenson39@ivytech.edu <JBJavaFall2019>
2019-09-13 11:38:32 -0400

    M02-03 Real World assignment, basic structure only

---
+++ M02-03 Real World Assignment/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int year = input.nextInt();
+
+
+
+
+
+    }
+}
